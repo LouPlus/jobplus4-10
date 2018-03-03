e36560d388eb4af3f3ece8295955fa64ca68ff2c
mumu223 <sszhao223@163.com>
2018-03-03 17:19:14 +0800

    #4 app.py

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,14 +1,20 @@
 from flask import Flask,render_template
 from jobplus.config import configs
-from jobplus.models import db,User,Job,Company
+from jobplus.models import db,User,Job,Company,Delivery
  
+def register_blueprint(app):
+    from .handlers import front,admin,user,job,company,test
+    app.register_blueprint(front)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+    app.register_blueprint(job)
+    app.register_blueprint(company)
+    app.register_blueprint(test)
+
 def create_app(config):
    app=Flask(__name__)
    app.config.from_object(configs.get(config))
    db.init_app(app)
-    
-   @app.route('/')
-   def index():
-       return render_template('index.html')
+   register_blueprints(app)
    return app
 
