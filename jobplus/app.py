bf99916886ab25c0a0442ecd5d53ee0155fb667e
mumu223 <34957506+mumu223@users.noreply.github.com>
2018-03-03 16:06:40 +0800

    app.py

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,14 +1,14 @@
 from flask import Flask,render_template
-  2 from jobplus.config import configs
-  3 from jobplus.models import db,User,Job,Company
-  4 
-  5 def create_app(config):
-  6     app=Flask(__name__)
-  7     app.config.from_object(configs.get(config))
-  8     db.init_app(app)
-  9     
- 10     @app.route('/')
- 11     def index():
- 12         return render_template('index.html')
- 13     return app
+from jobplus.config import configs
+from jobplus.models import db,User,Job,Company
+ 
+def create_app(config):
+   app=Flask(__name__)
+   app.config.from_object(configs.get(config))
+   db.init_app(app)
+    
+   @app.route('/')
+   def index():
+       return render_template('index.html')
+   return app
 
