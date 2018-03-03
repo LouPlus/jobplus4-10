1e5fb3157145f98198d170a5928eba034bc94b60
mumu223 <sszhao223@163.com>
2018-03-03 10:30:56 +0800

    #4 首页

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,1 +1,14 @@
+from flask import Flask,render_template
+  2 from jobplus.config import configs
+  3 from jobplus.models import db,User,Job,Company
+  4 
+  5 def create_app(config):
+  6     app=Flask(__name__)
+  7     app.config.from_object(configs.get(config))
+  8     db.init_app(app)
+  9     
+ 10     @app.route('/')
+ 11     def index():
+ 12         return render_template('index.html')
+ 13     return app
 
--- manage.py
+++ manage.py
@@ -1,1 +1,6 @@
+from jobplus.app import create_app
 
+app=create_app('development')
+
+if __name__=='__main__':
+    app.run()
