83d7ebe33b9a1bddd10632176e924bb0c2e49907
mumu223 <34957506+mumu223@users.noreply.github.com>
2018-03-04 11:50:31 +0800

    base.html models.py (#8)
    * fix #1 add info to README
    
    * Create models.py
    
    * 提交Flask基本目录结构
    
    * Delete models.py
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * Delete handlers
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * Delete register.py
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * Delete userregister.py
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * 提交Flask基本目录结构
    
    * Delete models。py
    
    * Create models.py
    
    * fix #5 add .gitignore file
    
    * Delete app,py
    
    * Create app.py
    
    * fix #6 完成页面的设计
    
    * #4 首页
    
    * #4 依赖包
    
    * fix #6 完善基本页面
    
    * 配置
    
    * 完善数据表信息
    
    * 配置文件
    
    * 完善数据表信息
    
    * app.py
    
    * 首页
    
    * fix #6 完善基本页面信息
    
    * 创建蓝图
    
    * #4 app.py
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * 基本目录结构＃4
    
    * Update config.py

--- jobplus/config.py
+++ jobplus/config.py
@@ -1,1 +1,18 @@
+class BaseConfig(object):
+    '''p配置基类'''
+    SECRET_KEY='make sure to set a very secret key'
+class DevelopmentConfig(BaseConfig):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+
+class ProductionConfig(BaseConfig):
+    pass
+class TestingConfig(BaseConfig):
+    pass
+
+configs={
+    'development':DevelopmentConfig,
+    'production':ProductionConfig,
+    'testing':TestingConfig
+}
 
--- jobplus/handlers/__init__.py
+++ jobplus/handlers/__init__.py
@@ -1,1 +1,7 @@
+from .front import front
+from .admin import admin
+from .user import user
+from .test import test
+from .company import company
+from .job import job
 
