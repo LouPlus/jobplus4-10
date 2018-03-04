91875eb3aca7eb787a51094f2c406ed8b9dcd072
mumu223 <sszhao223@163.com>
2018-03-04 10:45:50 +0800

    基本目录结构＃4

--- jobplus/config.py
+++ jobplus/config.py
@@ -1,1 +1,21 @@
+<<<<<<< HEAD
+=======
+class BaseConfig(object):
+    '''p配置基类'''
+    SECRET_KEY='make sure to set a very secret key'
+class DevelopmentConfig(BaseConfig):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI='mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+
+class ProductionConfig(BaseConfig):
+    pass
+class TestingConfig(BaseConfig):
+    pass
+
+configs={
+    'develpoment':DevelopmentConfig,
+    'production':ProductionConfig,
+    'testing':TestingConfig
+}
+>>>>>>> dev
 
--- jobplus/handlers/__init__.py
+++ jobplus/handlers/__init__.py
@@ -1,1 +1,8 @@
+from .front import front
+from .admin import admin
+from .user import user
+from .test import test
+from .company import company
+from .job import job
+
 
